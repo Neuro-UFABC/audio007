7afda7c149ed838997daf6314b069969e370b0c2
Bóris Marin <borismarin@gmail.com>
2025-12-17 16:52:22 -0300

    refatorizando

--- src/audio007/audio_utils.py
+++ src/audio007/audio_utils.py
@@ -3,6 +3,7 @@ import sounddevice as sd
 from scipy.io.wavfile import write, read
 from scipy.signal import convolve
 import numpy as np
+import pyloudnorm as pyln
 
 sd.default.latency = [0.1, 0.1]
 
@@ -79,6 +80,21 @@ def grava_binaural(segundos, fname, fs = 44100):
 ##############################
 # Refatorando
 
+def nivel(som, taxa=None):
+    if type(som) == str:
+        rate, data = read(som)
+    else:
+        data = som
+        rate = taxa
+    meter = pyln.Meter(rate)
+    return meter.integrated_loudness(data)
+
+
+def ganho_normalizador(estimulo, referencia, taxa=None):
+    delta = nivel(referencia, taxa) - nivel(estimulo, taxa)
+    return np.power(10.0, delta/20.0)
+
+
 def wavfile_pra_array(filename):
     taxa, dados = read(filename)
 
@@ -90,7 +106,7 @@ def wavfile_pra_array(filename):
     return taxa, dados
 
 def centra_normaliza(dados):
-    print('Centralizando cada canal (não muda ILD!)')
+    #print('Centralizando cada canal (não muda ILD!)')
     dados = dados - np.mean(dados, axis=0, keepdims=True)
 
     dado_max = np.abs(dados).max() 
@@ -104,6 +120,7 @@ def filtra(dados, filtro):
     return np.c_[convolve(dados[:,0], filtro[:,0], mode='full', method='auto'),
                       convolve(dados[:,1], filtro[:,1], mode='full', method='auto')]
 
+
 def _tocagrava(entrada, saida, filtro=None, ganho=[1,1]):
     taxa_entrada, est_array = wavfile_pra_array(entrada)
 
@@ -112,7 +129,7 @@ def _tocagrava(entrada, saida, filtro=None, ganho=[1,1]):
         if taxa_filtro != taxa_entrada:
             print('Taxa de amostragem da entrada diferente da do filtro! Desisto!!')
             return 1
-        print(filtro_array)
+        #print(filtro_array)
         est_array = filtra(est_array, filtro_array)
 
     est_array *= ganho
@@ -128,6 +145,60 @@ def _tocagrava(entrada, saida, filtro=None, ganho=[1,1]):
 
 
 
+def _grava_binaural(segundos, fname, fs = 44100): 
+
+    if fname is None:
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        fname = f'gravacao-{timestr}.wav'
+    print(f'Começando a tocar {entrada}. Duração:{segundos/fs: .3f}s')
+    rec = sd.rec(int(segundos * fs), samplerate=fs, channels=2, format='float32')
+    sd.wait() 
+    write(fname, fs, rec)
+    print(f'Gravação concluída. Salvo arquivo {fname}.')
+
+def _toca(dados_wav, lado='ambos', taxa=None, filtro=None, ganho=[1,1]):
+    if lado == 'ambos':
+        lmap = [1,2]
+    elif lado == 'esq':
+        lmap = 1
+    elif lado == 'dir':
+        lmap = 2
+    else:
+        raise ValueError('O argumento `lado` deve ser: "esq", "dir" ou "ambos"(padrão)')
+
+    # TODO: melhor fazer isso só uma vez, arquivos podem ser longos
+    if isinstance(dados_wav, str): # le do arquivo
+        taxa_wav, est_array = read(dados_wav)
+    else:
+        taxa_wav = taxa #se for um array, tem que passar a taxa
+        est_array = dados_wav
+
+    # TODO: isso fazia sentido para grilos, mas destrói a psicofísica!
+    #if len(dados.shape) > 1:
+    #    dados = dados.mean(axis=1).astype(dados.dtype)  # se for estéreo, mixa
+    #    print('Áudio estéreo! Tomando média dos canais.')
+
+    if taxa is None:
+        taxa = taxa_wav
+    else:
+        taxa = 44100
+   
+    if filtro:
+        taxa_filtro, filtro_array = wavfile_pra_array(filtro)
+        if taxa_filtro != taxa:
+            print('Taxa de amostragem da entrada diferente da do filtro! Desisto!!')
+            return 1
+        #print(filtro_array)
+        est_array = filtra(est_array, filtro_array)
+
+    est_array *= ganho
+    est_array = centra_normaliza(est_array) 
+
+    duracao = len(est_array)
+    print(f'Começando a tocar {dados_wav}. Duração:{duracao/taxa : .3f}s')
+
+    gravacao = sd.playrec(est_array, taxa, channels=2, blocking=True, dtype='float32')
+    sd.wait()
 
 
 
